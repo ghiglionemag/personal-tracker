156136f13ca9890595ee0bfba1ac4ef0d6e57b66
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-02 11:30:08 -0300

    2do commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import "./components/text";
-import "./components/nav";
+import "./components/text"
+import "./components/nav"
 import "./components/to-do-item";
 import { initHomePage } from "./pages/home";
 import { state } from "./state";
 
 function main() {
-  
   const rootElement = document.querySelector(".root");
   initHomePage(rootElement);
+  state.initState();
 }
 main();
