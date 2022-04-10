456fbd580391aea12a95a5e683e334d3023bc980
Agustina Ghiglione <ghiglionemag@gmail.com>
2022-04-10 00:05:30 -0300

    Tratando de mejorar el diseño, todavía falta la capa de persistencia

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -7,24 +7,22 @@ export class Button extends HTMLElement {
     const shadow = this.attachShadow({ mode: "open" });
 
     const button = document.createElement("button");
-    button.className = "root";
+    button.className = "button";
 
     const style = document.createElement("style");
     style.innerHTML = `
-            .root{
-                min-width: 100pxpx; 
-                line-height: 55px;
+            .button{
+                min-width: 200px;
                 margin: 12px 0px;
-                border-radius: 20px;
-                font-size: 22px;
+                font-size: 15px;
                 text-align: center;
                 font-family: 'Anton', sans-serif;
+                border-radius: 5px;
+                height: 40px;
+                border: 0;
             }
             @media (min-width: 899px){
-                .root{
-                  margin-left: 10px;
-                  
-                }
+               
               }
         `;
 
